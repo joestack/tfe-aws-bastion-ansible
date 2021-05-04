65569861d6f8a95c7e71b6431796aa35f8861985
joestack <joern@joern-online.de>
2021-05-04 12:57:03 +0200

    more outputs

--- output.tf
+++ output.tf
@@ -10,6 +10,18 @@ output "ELB_public_DNS" {
   value = "${aws_route53_record.elb.name}.${var.dns_domain}"
 }
 
+output "inventory" {
+  value = data.template_file.ansible_skeleton.rendered
+}
+
+output "ansible_hosts" {
+  value = data.template_file.ansible_hosts.rendered
+}
+
+output "web_node_ips" {
+  value = aws_instance.web_nodes.*.private_ip
+}
+
 # output "ELB_AWS_internal_DNS" {
 #   value = aws_elb.web-elb.dns_name
 # }
