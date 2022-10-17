e7ebdb1e7cc5063ddd5bdc081d6a1413b6743145
joestack <joern@joern-online.de>
2022-10-17 18:59:28 +0200

    test new nat_gateway 1.2

--- main.tf
+++ main.tf
@@ -61,8 +61,13 @@ data "aws_ami" "ubuntu" {
 #   }
 # }
 
+
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+}
+
 resource "aws_nat_gateway" "nat" {
-  #allocation_id = aws_eip.example.id
+  allocation_id = aws_eip.nat_gateway.id
   subnet_id     = aws_subnet.dmz_subnet.id
 
   tags = {
