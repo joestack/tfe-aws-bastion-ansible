eb7349065f87635cde8526bbff6d5f4d38d72d64
JoeStack <joern@hashicorp.com>
2024-09-13 09:04:24 +0200

    ubuntu image update

--- main.tf
+++ main.tf
@@ -20,7 +20,9 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    #values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+
   }
 
   filter {
